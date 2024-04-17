80169697c7508a9916110a6219fd04bd5efb6c8b
leciet <148856206+leciet@users.noreply.github.com>
2024-04-17 11:36:17 +0200

    Update code.R

--- code.R
+++ code.R
@@ -1,4 +1,5 @@
 
+
 # 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
 
 #https://github.com/leciet/analyse-de-donn-es.git
@@ -8,10 +9,9 @@ rm(list=ls())
 library(tidyverse)
 library(Factoshiny)
 library(FactoMineR)
-
 library(readr)
 data <- read_delim("jeu de données_portugal.csv", 
-                                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
+                   delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
 data$school=as.factor(data$school)
 data$course=as.factor(data$course)
@@ -63,7 +63,7 @@ levels(data$nursery) <- c("nurs-YES","nurs-NO")
 
 data$higher=as.factor(data$higher)
 levels(data$higher)
-levels(data$higher) <- c("high-YES","high-NO")
+levels(data$higher) <- c("highEdu-YES","highEdu-NO")
 
 data$internet=as.factor(data$internet)
 levels(data$internet)
@@ -75,27 +75,27 @@ levels(data$romantic) <- c("rom-YES","rom-NO")
 
 data$famrel=as.factor(data$famrel)
 levels(data$famrel)
-levels(data$famrel) <- c("famrel-vl","famrel-l","famrel-med","famrel-hg","famrel-vhg")
+levels(data$famrel) <- c("famrel-1","famrel-2","famrel-3","famrel-4","famrel-5")
 
 data$freetime=as.factor(data$freetime)
 levels(data$freetime)
-levels(data$freetime) <- c("freet-vl","freet-l","freet-med","freet-hg","freet-vhg")
+levels(data$freetime) <- c("freet-1","freet-2","freet-3","freet-4","freet-5")
 
 data$goout=as.factor(data$goout)
 levels(data$goout)
-levels(data$goout) <- c("goout-vl","goout-l","goout-med","goout-hg","goout-vhg")
+levels(data$goout) <- c("go_out-1","go_out-2","go_out-3","go_out-4","go_out-5")
 
 data$Dalc=as.factor(data$Dalc)
 levels(data$Dalc)
-levels(data$Dalc) <- c("Dalc-vl","Dalc-l","Dalc-med","Dalc-hg","Dalc-vhg")
+levels(data$Dalc) <- c("Dalc-1","Dalc-2","Dalc-3","Dalc-4","Dalc-5")
 
 data$Walc=as.factor(data$Walc)
 levels(data$Walc)
-levels(data$Walc) <- c("Walc-vl","Walc-l","Walc-med","Walc-hg","Walc-vhg")
+levels(data$Walc) <- c("Walc-1","Walc-2","Walc-3","Walc-4","Walc-5")
 
 data$health=as.factor(data$health)
 levels(data$health)
-levels(data$health) <- c("health-vl","health-l","health-med","health-hg","health-vhg")
+levels(data$health) <- c("health-1","health-2","health-3","health-4","health-5")
 
 
 
